8fd389b3637e79d56e6fc83a66bc55ccd29e9e19
MUHAMMADSHEHZAD01 <110229877+MUHAMMADSHEHZAD01@users.noreply.github.com>
2024-09-05 16:02:33 +0500

    Update and rename moel loading.py to model loading.py
    main 2

--- model loading.py
+++ model loading.py
@@ -2,22 +2,23 @@ import pandas as pd
 import joblib
 from sklearn.metrics import accuracy_score
 
-model = joblib.load('k-Nearest Neighbours.pkl')
+model = joblib.load('K-Nearest Neighborsmodel.pkl')  # Ensure this is the model, not data
 
 # Load the test data
-test_data = pd.read_csv('content/MLOps-Task-1B/mobile_price_range_data.csv')
+test_data = pd.read_csv('mobile_price_range_data.csv')
 
 # Assuming the last column is the target
 X_test = test_data.iloc[:, :-1]
 y_test = test_data.iloc[:, -1]
 
 # Generate test predictions
-# Make predictions on the test data
 y_pred = model.predict(X_test)
 
 # Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
 
-#save accuracy to a file
+# Save accuracy to a file
 with open('accuracy.txt', 'w') as file:
-    file.write(str(accuracy))
+    file.write(f'Accuracy: {accuracy}')
+
+print(f'Accuracy: {accuracy}')
